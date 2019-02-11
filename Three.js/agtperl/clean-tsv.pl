82f53d743ffba1bdd5268fdf7181e002bfeb5e08
Joe Golden <joe@triangul.us>
2019-02-11 14:07:51 -0500

    Add lable for 8.0 O2 level.

--- Three.js/agtperl/clean-tsv.pl
+++ Three.js/agtperl/clean-tsv.pl
@@ -4,6 +4,7 @@
 use POSIX;
 
 my $header = <>;
+print "$header\n";
 while(<>){
 	if (/^
 		([^\t]+)	# time
@@ -25,8 +26,10 @@ while(<>){
 		/x){
 		my ($date,$twi,$two,$o2,$z1,$z2,$z3,$z4) = 
 			($1,floor($2),floor($3),$4,$5,$6,$7,$8);
-		print "Match on date: $date\ttwi: $twi\ttwo: $two\n";
-		print "\tO2: $o2\tz1: $z1\tz2: $z2\tz3: $z3\tz4: $z4\n";
+			#print "Match on date: $date\ttwi: $twi\ttwo: $two\n";
+			#print "\tO2: $o2\tz1: $z1\tz2: $z2\tz3: $z3\tz4: $z4\n";
+		print "$date\t$twi\t$two\t";
+		print "$o2\t$z1\t$z2\t$z3\t$z4\n";
 	} elsif (/^
 		([^\t]+)	# time
 		\t
