53264b7d8197dbb23a35ee6a4904b3b24ea30842
Joe Golden <joe@triangul.us>
2019-02-14 15:12:30 -0500

    Adjust for json.

--- Three.js/agtperl/clean-tsv.pl
+++ Three.js/agtperl/clean-tsv.pl
@@ -8,7 +8,7 @@ use strict;
 my $header = <>;
 my $count=0;
 #print "$header\n";
-print "Time	O2	T1	T2	T3	T4\n";
+print "['Time', 'O2', 'T1', 'T2', 'T3', 'T4'],\n";
 # 'Time'  'O2'    'T1'    'T2'    'T3'    'T4'
 
 my $time;
@@ -45,13 +45,13 @@ while(<>){
 			my ($h,$m) = ($1,$2);
 			my $mins = 60*$h + $m;
 			#$time =~ s/://;
-			print "$mins\t";
+			print "[$mins, ";
 		} else {
 			die('Bad Format');
 		}
 		$o2 *= 10;
 		$o2 = floor( $o2 );
-		print "$o2\t$z1\t$z2\t$z3\t$z4\n";
+		print "$o2, $z1, $z2, $z3, $z4],\n";
 	} elsif (/^
 		([^\t]+)	# time
 		\t
